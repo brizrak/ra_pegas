06f7ef55ad73a10026ad44c6fff6e6d1603e68e5
Vladimir Nenashev <Vladimirnenasev8700@gmail.com>
2024-04-05 00:17:20 +0700

    some structuring again

--- wagtail/ra_pegas/home/models.py
+++ wagtail/ra_pegas/home/models.py
@@ -6,16 +6,17 @@ from wagtail.fields import RichTextField
 
 
 class HomePage(Page):
-    cat_image = RichTextField(
-        blank=True,
-        null=True,
-    )
-    cats_image = RichTextField(
-        blank=True,
-        null=True,
-    )
-
-    content_panels = Page.content_panels + [
-        FieldPanel('cat_image'),
-        FieldPanel('cats_image'),
-    ]
+    pass
+    # cat_image = RichTextField(
+    #     blank=True,
+    #     null=True,
+    # )
+    # cats_image = RichTextField(
+    #     blank=True,
+    #     null=True,
+    # )
+    #
+    # content_panels = Page.content_panels + [
+    #     FieldPanel('cat_image'),
+    #     FieldPanel('cats_image'),
+    # ]
